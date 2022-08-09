81ede7f2a677ef56dbd9765a234044ac29ae7829
ugur-sa <sadiklarugur@gmail.com>
2022-08-09 19:14:13 +0200

    Add devServer

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,13 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000
+  },
   module: {
     rules: [
       {
