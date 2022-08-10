c54a2c7bfa81ee5af8cff61f478fe57ff04587d8
ugur-sa <sadiklarugur@gmail.com>
2022-08-10 23:00:08 +0200

    Add module logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-function printToConsole() {
-  console.log('restaurant page with webpack test');
-}
+import './styles.css';
+import Header from './modules/header';
+import Main from './modules/main';
+import Footer from './modules/footer';
 
-printToConsole();
+const body = document.querySelector('body');
+
+const content = document.createElement('div');
+content.setAttribute('id', 'content');
+
+const header = Header();
+const main = Main();
+const footer = Footer();
+
+content.appendChild(header);
+content.appendChild(main);
+content.appendChild(footer);
+body.appendChild(content);
+
