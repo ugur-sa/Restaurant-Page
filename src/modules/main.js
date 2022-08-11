441b7945501e480420d285f47455d441254595b7
ugur-sa <sadiklarugur@gmail.com>
2022-08-11 23:08:22 +0200

    Add main module layout

--- src/modules/main.js
+++ src/modules/main.js
@@ -1,7 +1,23 @@
 function Main() {
   const main = document.createElement('div');
   main.classList.add('main');
-  main.innerHTML = 'main';
+
+  const about = document.createElement('div');
+  about.classList.add('about');
+
+  const chefImage = document.createElement('div');
+  chefImage.classList.add('chef');
+
+  const welcome = document.createElement('div');
+  welcome.classList.add('welcome');
+
+  const welcomeText = document.createElement('p');
+  const welcomeTextContent = 'Hello, my name is ______.</br>This restaurant was founded in 1930</br>and is cooking the same dishes as 90 years ago';
+  welcomeText.innerHTML = welcomeTextContent;
+  welcome.appendChild(welcomeText);
+  main.appendChild(about);
+  about.appendChild(chefImage);
+  about.appendChild(welcome);
 
   return main;
 }
